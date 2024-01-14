a9f4a90ae3cef8e81a6c1ce955f1b15b51fd1b00
KSBMKunal <playtel59@gmail.com>
2024-01-14 21:33:39 +0530

    db.js file correction

--- config/db.js
+++ config/db.js
@@ -4,6 +4,6 @@ const mongoose = require('mongoose');
 const dbURI = 'mongodb+srv://rajivpathak199:rajiv199@cluster0.m6krzpo.mongodb.net/angelLudo';
 
 module.exports = function dbConnection() {
-    mongoose.connect(dbURI, { useNewUrlParser: true, useUnifiedTopology: true });
+    mongoose.connect(dbURI,  { useNewUrlParser: true, useUnifiedTopology: true });
     console.log("connected to the database");
 }
