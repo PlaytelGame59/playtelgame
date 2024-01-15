ddc707ecf1f40c65c521c0ac3e82ae13068e22c6
KSBMKunal <playtel59@gmail.com>
2024-01-15 12:08:46 +0530

    changes in db.js

--- config/db.js
+++ config/db.js
@@ -4,6 +4,8 @@ const mongoose = require('mongoose');
 const dbURI = 'mongodb+srv://rajivpathak199:rajiv199@cluster0.m6krzpo.mongodb.net/angelLudo';
 
 module.exports = function dbConnection() {
-    mongoose.connect(dbURI,  { useNewUrlParser: true, useUnifiedTopology: true });
+    mongoose.connect(dbURI,  { useNewUrlParser: true,
+        useUnifiedTopology: true,
+        useFindAndModify: false, });
     console.log("connected to the database");
 }
