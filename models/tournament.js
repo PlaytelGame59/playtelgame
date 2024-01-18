6d7e388e5bb911cc3ef87e4f1fae23184cb94020
KSBMKunal <playtel59@gmail.com>
2024-01-18 14:37:41 +0530

    changes done

--- models/tournament.js
+++ models/tournament.js
@@ -2,10 +2,6 @@ const mongoose = require('mongoose')
 
 const TournamentSchema = mongoose.Schema({
 
-// tournament_id, player_id, bonus_amount, play_amount, players (Optional), 
-// use_of (Optional), notes (optional), refund (Optional)
-
-
     tournamentName: { 
         type: String,
         default:"" },
@@ -43,8 +39,11 @@ const TournamentSchema = mongoose.Schema({
 
     tournamentInterval: { type: String }, 
     tournamentType: { type: String }, 
-    tournamentStatus: { type: String }
-})    
+    tournamentStatus: { type: String },
+}, {
+    collection: 'Players',
+    timestamps: true
+});
 
 const Tournament = mongoose.model('tournament', TournamentSchema)
 
